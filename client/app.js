32569e272430620aa9243212b224429bf03767d9
Martin Baer <martin@martinbaer.dev>
2023-05-13 13:46:33 +1000

    copied Denis's dao.sol into main and added more to client

--- client/app.js
+++ client/app.js
@@ -1,28 +1,61 @@
-import { ethers } from "./ethers.min.js";
+// import { ethers } from "https://cdn.ethers.io/lib/ethers-5.2.esm.min.js";
 
-document.addEventListener('DOMContentLoaded', async () => {
+const CONTRACT_ADDRESS = "0x6F0fDFBa064627156e6c2582a6bbb75D83907476";
+// read from contract_abi.json
 
-    if (typeof window.ethereum !== 'undefined') {
-        const provider = new ethers.BrowserProvider(window.ethereum);
-        const connectButton = document.getElementById('connectButton');
-        const accountElement = document.getElementById('account');
+console.log(ethers);
 
-        connectButton.addEventListener('click', async () => {
+document.addEventListener("DOMContentLoaded", async () => {
+
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const connectMetamaskButton = document.getElementById("connectMetamaskButton");
+
+        connectMetamaskButton.addEventListener("click", async () => {
             try {
                 console.log("connected: ", window.ethereum.isConnected())
                 // Request account access
-                await window.ethereum.request({ method: 'eth_requestAccounts' });
+                await window.ethereum.request({ method: "eth_requestAccounts" });
 
                 // Fetch account from provider
                 const signer = await provider.getSigner();
                 console.log("signer: ", signer);
                 const account = await signer.getAddress();
-                accountElement.innerHTML = `Connected: ${account}`;
+
+                // hide connect button
+                connectMetamaskButton.style.display = "none";
+                // change text of connectMetamaskLabel
+                const connectMetamaskLabel = document.getElementById("connectMetamaskLabel");
+                connectMetamaskLabel.innerHTML = `Connected to account: ${account}`;
+
+                // Fetch contract
+                const contract_abi_file = await fetch("./contract_abi.json");
+                const contract_abi = await contract_abi_file.json();
+
+                // create contract instance
+                const contract = new ethers.Contract(CONTRACT_ADDRESS, contract_abi, signer);
+                console.log("contract: ", contract);
+                // const contract_owner = await contract.owner();
+                // console.log("contract_owner: ", contract_owner);
+
+                // call viewMemberNames
+                const members = await contract.viewMemberNames();
+                console.log("members: ", members);
+                // create list of members
+                const membersList = document.getElementById("membersList");
+                members.forEach(member => {
+                    const li = document.createElement("li");
+                    li.innerHTML = member;
+                    membersList.appendChild(li);
+                }
+                );
+
             } catch (error) {
                 console.error(error);
             }
         });
+
     } else {
-        console.log('No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.');
+        console.log("No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.");
     }
 });
--- smart_contract/dao.sol
+++ smart_contract/dao.sol
@@ -1,139 +1,363 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.9.0;
+/* 
+    SPDX-License-Identifier: Unlicensed 
 
-contract UQCryptoClubDAO {
-    struct Member {
-        string name;
-        uint256 voteWeight;
-        uint256 lastActive;
-        bool approved;
-    }
+*/
 
-    struct Decision {
+pragma solidity ^0.8.17;
+
+contract cryptoDAO {
+    // structs
+    struct ClubEvent {
+        uint256 id;
+        string title;
         string description;
-        mapping(address => uint256) votes;
-        address[] voters;
-        uint256 endTime;
-        bool active;
-    }
-
-    struct Event {
-        string name;
-        string description;
-        uint256 time;
-        address[] attendees;
         address creator;
-        bool active;
+        address[] members;
+        bool stillOpen;
     }
 
-    mapping(address => Member) public members;
-    address[] public memberAddresses;
-
-    Decision[] public decisions;
-    Event[] public events;
-
-    // Membership functions
-    function joinDAO(string memory name) public {
-        require(!members[msg.sender].approved, "Already a member.");
-        members[msg.sender] = Member(name, 0, block.timestamp, false);
-        memberAddresses.push(msg.sender);
+    struct AddMemberVote {
+        address newMember;
+        string name;
+        uint256 votes;
+        mapping(address => uint256) votesCastByMembers;
     }
 
-    function approveMember(address member) public {
-        require(members[msg.sender].approved, "Only members can approve.");
-        require(!members[member].approved, "Member already approved.");
-        members[member].approved = true;
+    struct ClubChange {
+        uint256 id;
+        string description;
+        uint256 votes;
+        bool accepted;
     }
 
-    function checkActivity() public {
-        for (uint256 i = 0; i < memberAddresses.length; i++) {
-            if (
-                block.timestamp - members[memberAddresses[i]].lastActive >=
-                90 days
-            ) {
-                delete members[memberAddresses[i]];
-            }
+    struct ClubPayment {
+        uint256 id;
+        address recipient;
+        uint256 amount;
+        string description;
+        uint256 votes;
+        bool accepted;
+    }
+
+    struct Election {
+        uint256 electionId;
+        uint256 votesToStart;
+        uint256 startTime;
+        uint256 endTime;
+        bool votingEnabled;
+        mapping(address => uint256) presidentVotes;
+    }
+    // end structs
+
+    // JOINING THE CLUB
+
+    mapping(address => AddMemberVote) public approvalVotes;
+    event joinRequest(address member, string);
+    event newMemberAccepted(string name);
+
+    // MEMBERS AND VOTING
+    address[] members;
+    mapping(address => string) memberAddressToName;
+    mapping(address => uint256) public userVotes;
+    uint256 public totalVotes = 0;
+
+    // CHANGES
+    mapping(uint256 => ClubChange) clubChanges; // id => clubChange
+    mapping(uint256 => mapping(address => uint256)) votesCastByMembersForChange; // change id => (voter => votes cast by voter)
+    uint256 changesCount = 0;
+    event changeAccepted(uint256 id);
+
+    // EVENTS
+    ClubEvent[] events;
+
+    // ELECTIONS
+    address president;
+    Election election;
+    uint256 electionId = 0;
+
+    // MONEY
+    ClubPayment[] public clubPayments; //
+    mapping(uint256 => mapping(address => uint256)) votesCastByMembersForPayment; // payment id => (voter => votes cast by voter)
+    uint256 public paymentsCount = 0;
+    event paymentAccepted(uint256 id);
+
+    modifier memberOnly() {
+        require(userVotes[msg.sender] > 0, "you are not in the club");
+        _;
+    }
+
+    constructor() {
+        members.push(msg.sender);
+        president = msg.sender;
+        memberAddressToName[msg.sender] = "satoshi";
+        userVotes[msg.sender] = 1;
+        totalVotes = 1;
+    }
+
+    function viewMemberName(address user) public view returns (string memory) {
+        return memberAddressToName[user];
+    }
+
+    function viewMemberAddresses() public view returns (address[] memory) {
+        return members;
+    }
+
+    function viewMemberNames() public view returns (string[] memory) {
+        string[] memory names = new string[](members.length);
+        for (uint i = 0; i < members.length; i++) {
+            names[i] = memberAddressToName[members[i]];
+        }
+        return names;
+    }
+
+    // ===================================    JOINING CLUB    =================================== //
+
+    function requestToJoin(string memory name) external payable {
+        require(msg.value >= 10 ** 15 wei); // user must pay >10^15 wei == 0.001eth to request to join club, to prevent spam (works as entrance fee)
+        approvalVotes[msg.sender].newMember = msg.sender;
+        approvalVotes[msg.sender].name = name;
+        approvalVotes[msg.sender].votes = 0;
+        emit joinRequest(msg.sender, name);
+    }
+
+    function voteToAddMember(address newMember) external memberOnly {
+        // if the current member has not yet cast a vote to induct the new member
+        require(
+            approvalVotes[newMember].votesCastByMembers[msg.sender] == 0,
+            "you have already voted to add this user"
+        );
+        // cast the votes
+        approvalVotes[newMember].votes += userVotes[msg.sender];
+        approvalVotes[newMember].votesCastByMembers[msg.sender] = userVotes[
+            msg.sender
+        ];
+
+        if (
+            approvalVotes[newMember].votes > totalVotes / 2 &&
+            userVotes[newMember] == 0
+        ) {
+            members.push(newMember);
+            memberAddressToName[newMember] = approvalVotes[newMember].name;
+            userVotes[newMember] = 1;
+            totalVotes++;
+            emit newMemberAccepted(memberAddressToName[newMember]);
         }
     }
 
-    // Decision functions
-    function createDecision(
-        string memory description,
-        uint256 duration
-    ) public {
-        require(
-            members[msg.sender].approved,
-            "Only members can create decisions."
-        );
-        decisions.push();
-        decisions[decisions.length - 1].description = description;
-        decisions[decisions.length - 1].endTime = block.timestamp + duration;
-        decisions[decisions.length - 1].active = true;
-    }
+    // ===================================    CLUB EVENTS    =================================== //
 
-    function vote(uint256 decisionId, uint256 option) public {
-        require(members[msg.sender].approved, "Only members can vote.");
-        require(decisions[decisionId].active, "Decision is no longer active.");
-        require(
-            decisions[decisionId].endTime > block.timestamp,
-            "Voting time has elapsed."
-        );
-        decisions[decisionId].votes[msg.sender] = option;
-        decisions[decisionId].voters.push(msg.sender);
-    }
-
-    // Event functions
+    // create a new event for the club which contains the creator, attendees, and whether it's still open.
     function createEvent(
-        string memory name,
-        string memory description,
-        uint256 time
-    ) public {
-        require(
-            members[msg.sender].approved,
-            "Only members can create events."
-        );
-        events.push(
-            Event(name, description, time, new address[](0), msg.sender, true)
-        );
+        string memory title,
+        string memory description
+    ) external memberOnly returns (ClubEvent memory) {
+        ClubEvent memory newEvent;
+        newEvent.title = title;
+        newEvent.description = description;
+        newEvent.creator = msg.sender;
+        newEvent.stillOpen = true;
+        events.push(newEvent);
+        return newEvent;
     }
 
-    function attendEvent(uint256 eventId) public {
-        require(
-            members[msg.sender].approved,
-            "Only members can attend events."
-        );
-        require(events[eventId].active, "Event is no longer active.");
-        events[eventId].attendees.push(msg.sender);
-    }
-
-    function deleteEvent(uint256 eventId) public {
-        require(
-            events[eventId].creator == msg.sender,
-            "Only the event creator can delete the event."
-        );
-        events[eventId].active = false;
-    }
-
-    function removeAttendee(uint256 eventId, address attendee) public {
-        require(
-            events[eventId].creator == msg.sender,
-            "Only the event creator can remove attendees."
-        );
-        require(events[eventId].active, "Event is no longer active.");
-
-        // Get the list of attendees
-        address[] storage attendees = events[eventId].attendees;
-
-        for (uint i = 0; i < attendees.length; i++) {
-            if (attendees[i] == attendee) {
-                // Swap the attendee to remove with the last attendee
-                attendees[i] = attendees[attendees.length - 1];
-
-                // Delete the last attendee
-                attendees.pop();
-
-                break;
-            }
+    function joinEvent(uint256 id) external memberOnly {
+        require(userVotes[msg.sender] > 0, "you are not in the club");
+        require(events[id].stillOpen, "event closed");
+        require(events[id].stillOpen, "event closed");
+        events[id].members.push(msg.sender);
+        if (userVotes[events[id].creator] < 10) {
+            userVotes[events[id].creator]++; // increases event creators voting weight
+            totalVotes++;
         }
     }
+
+    function closeEvent(uint256 id) external memberOnly {
+        require(msg.sender == events[id].creator);
+        events[id].stillOpen = false;
+    }
+
+    function viewAllEvents() external view returns (ClubEvent[] memory) {
+        return events;
+    }
+
+    function viewOpenEvents() external view returns (ClubEvent[] memory) {
+        ClubEvent[] memory openEvents = new ClubEvent[](events.length);
+        uint count = 0;
+        for (uint i = 0; i < events.length; i++) {
+            if (events[i].stillOpen) {
+                openEvents[count] = events[i];
+                count++;
+            }
+        }
+        return openEvents;
+    }
+
+    function viewEventInfo(
+        uint256 id
+    ) external view returns (ClubEvent memory) {
+        return events[id];
+    }
+
+    function viewEventTitle(uint256 id) external view returns (string memory) {
+        return events[id].title;
+    }
+
+    function viewEventDescription(
+        uint256 id
+    ) external view returns (string memory) {
+        return events[id].description;
+    }
+
+    function viewEventAttendees(
+        uint256 id
+    ) external view returns (address[] memory) {
+        return events[id].members;
+    }
+
+    // ===================================    CLUB CHANGES    =================================== //
+
+    function proposeChange(string memory description) external memberOnly {
+        uint256 id = changesCount;
+
+        ClubChange memory newChange = ClubChange(
+            id,
+            description,
+            userVotes[msg.sender],
+            false
+        );
+
+        votesCastByMembersForChange[id][msg.sender] = userVotes[msg.sender];
+
+        clubChanges[id] = newChange;
+
+        changesCount++;
+    }
+
+    function voteOnChange(uint256 id) external memberOnly {
+        require(clubChanges[id].votes > 0, "change id does not exist!");
+        require(
+            votesCastByMembersForChange[id][msg.sender] == 0,
+            "you have already voted!"
+        );
+        require(!clubChanges[id].accepted, "change has already been accepted!");
+        clubChanges[id].votes += userVotes[msg.sender];
+        votesCastByMembersForChange[id][msg.sender] = userVotes[msg.sender];
+        if (clubChanges[id].votes > totalVotes / 2) {
+            clubChanges[id].accepted = true;
+            emit changeAccepted(id);
+        }
+    }
+
+    // PAYMENTS //
+
+    // struct ClubPayment {
+    //     uint256 id;
+    //     address recipient;
+    //     uint256 amount;
+    //     string paymentDescription;
+    //     uint256 votes;
+    //     bool accepted;
+    // }
+
+    function proposeSpendClubETH(
+        address recipient,
+        uint256 amount,
+        string memory description
+    ) external memberOnly {
+        uint256 id = paymentsCount;
+
+        ClubPayment memory newPayment = ClubPayment(
+            id,
+            recipient,
+            amount,
+            description,
+            userVotes[msg.sender],
+            false
+        );
+
+        votesCastByMembersForPayment[id][msg.sender] = userVotes[msg.sender];
+
+        clubPayments[id] = newPayment;
+
+        paymentsCount++;
+    }
+
+    function voteSpendClubETH(uint256 paymentId) external memberOnly {
+        require(
+            clubPayments[paymentId].votes > 0,
+            "payment id does not exist!"
+        );
+        require(
+            votesCastByMembersForPayment[paymentId][msg.sender] == 0,
+            "you have already voted!"
+        );
+        require(
+            !clubPayments[paymentId].accepted,
+            "payment has already been accepted!"
+        );
+        clubPayments[paymentId].votes += userVotes[msg.sender];
+        votesCastByMembersForPayment[paymentId][msg.sender] = userVotes[
+            msg.sender
+        ];
+        if (clubPayments[paymentId].votes > totalVotes / 2) {
+            clubPayments[paymentId].accepted = true;
+            (bool success, ) = clubPayments[paymentId].recipient.call{
+                value: clubPayments[paymentId].amount
+            }("a payment from the UQ crypto club DAO");
+            require(success);
+            emit paymentAccepted(paymentId);
+        }
+    }
+
+    function viewSpendETHProposalDescription(
+        uint256 proposalId
+    ) public view returns (string memory) {
+        return clubPayments[proposalId].description;
+    }
+
+    function viewSpendETHProposalVotes(
+        uint256 proposalId
+    ) public view returns (uint256) {
+        return clubPayments[proposalId].votes;
+    }
+
+    function viewSpendETHProposals()
+        public
+        view
+        returns (ClubPayment[] memory)
+    {
+        return clubPayments;
+    }
+
+    // function voteStartElection(bool start) external memberOnly {
+    //     if ()
+    //     election.
+    // }
+
+    // struct Election {
+    //     uint256 electionId;
+    //     uint256 votesToStart
+    //     uint256 startTime;
+    //     uint256 endTime;
+    //     bool votingEnabled;
+    //     mapping (address => uint256) presidentVotes;
+    // }
+
+    // function voteForPresident(address candidate) external memberOnly {
+
+    // }
+
+    // ===================================    VIEW CLUB INFO    =================================== //
+
+    // not needed bc fields are public
+
+    // // view the members of the club
+    // function viewMembers() public view returns (address[] memory) {
+    //     return members;
+    // }
+
+    // // view how many votes a member has
+    // function viewMemberVotes(address member) public view returns (uint256) {
+    //     return userVotes[member];
+    // }
 }
